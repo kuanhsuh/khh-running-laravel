9a6870a5f22628355bb64b8a0d77d521a1557ffc
dannyhuang <kuanhsuh@gmail.com>
2023-03-10 14:47:46 +0800

    update deploy sh

--- deploy.sh
+++ deploy.sh
@@ -7,21 +7,21 @@ php artisan down
 # update source code
 git pull
 # update PHP dependencies
-composer install --no-interaction --no-dev --prefer-dist
+# composer install --no-interaction --no-dev --prefer-dist
 # --no-interaction Do not ask any interactive question
 # --no-dev  Disables installation of require-dev packages.
 # --prefer-dist  Forces installation from package dist even for dev versions.
 # Clear the old cache
-php artisan clear-compiled
+#php artisan clear-compiled
 
 # Recreate cache
-php artisan optimize
+#php artisan optimize
 
 # Compile npm assets
-npm run prod
+npm run build
 
 # update database
-php artisan migrate --force
+php artisan migrate
 # --force  Required to run when in production.
 # stop maintenance mode
 php artisan up
