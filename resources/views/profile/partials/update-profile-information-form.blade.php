783552c629b589622bcbd99e072f44feaf2384eb
dannyhuang <kuanhsuh@gmail.com>
2023-03-11 20:31:41 +0800

    bug - fixed update profile select option

--- resources/views/profile/partials/update-profile-information-form.blade.php
+++ resources/views/profile/partials/update-profile-information-form.blade.php
@@ -64,46 +64,46 @@
 
             <label for="area_code">郵遞區號</label>
             <select name="area_code" id="area_code" class="mt-1 block w-full shadow-sm border-gray-300 rounded-md">
-                <option value="800" {{ old('area_code') == '800' ? 'selected' : '' }}>800新興區</option>
-                <option value="801" {{ old('area_code') == '801' ? 'selected' : '' }}>801前金區</option>
-                <option value="802" {{ old('area_code') == '802' ? 'selected' : '' }}>802苓雅區</option>
-                <option value="803" {{ old('area_code') == '803' ? 'selected' : '' }}>803鹽埕區</option>
-                <option value="804" {{ old('area_code') == '804' ? 'selected' : '' }}>804鼓山區</option>
-                <option value="805" {{ old('area_code') == '805' ? 'selected' : '' }}>805旗津區</option>
-                <option value="806" {{ old('area_code') == '806' ? 'selected' : '' }}>806前鎮區</option>
-                <option value="807" {{ old('area_code') == '807' ? 'selected' : '' }}>807三民區</option>
-                <option value="811" {{ old('area_code') == '811' ? 'selected' : '' }}>811楠梓區</option>
-                <option value="812" {{ old('area_code') == '812' ? 'selected' : '' }}>812小港區</option>
-                <option value="813" {{ old('area_code') == '813' ? 'selected' : '' }}>813左營區</option>
-                <option value="814" {{ old('area_code') == '814' ? 'selected' : '' }}>814仁武區</option>
-                <option value="815" {{ old('area_code') == '815' ? 'selected' : '' }}>815大社區</option>
+                <option value="800" {{ $user->area_code == '800' ? 'selected' : '' }}>800新興區</option>
+                <option value="801" {{ $user->area_code == '801' ? 'selected' : '' }}>801前金區</option>
+                <option value="802" {{ $user->area_code == '802' ? 'selected' : '' }}>802苓雅區</option>
+                <option value="803" {{ $user->area_code == '803' ? 'selected' : '' }}>803鹽埕區</option>
+                <option value="804" {{ $user->area_code == '804' ? 'selected' : '' }}>804鼓山區</option>
+                <option value="805" {{ $user->area_code == '805' ? 'selected' : '' }}>805旗津區</option>
+                <option value="806" {{ $user->area_code == '806' ? 'selected' : '' }}>806前鎮區</option>
+                <option value="807" {{ $user->area_code == '807' ? 'selected' : '' }}>807三民區</option>
+                <option value="811" {{ $user->area_code == '811' ? 'selected' : '' }}>811楠梓區</option>
+                <option value="812" {{ $user->area_code == '812' ? 'selected' : '' }}>812小港區</option>
+                <option value="813" {{ $user->area_code == '813' ? 'selected' : '' }}>813左營區</option>
+                <option value="814" {{ $user->area_code == '814' ? 'selected' : '' }}>814仁武區</option>
+                <option value="815" {{ $user->area_code == '815' ? 'selected' : '' }}>815大社區</option>
 
-                <option value="820" {{ old('area_code') == '820' ? 'selected' : '' }}>820岡山區</option>
-                <option value="821" {{ old('area_code') == '821' ? 'selected' : '' }}>821路竹區</option>
-                <option value="822" {{ old('area_code') == '822' ? 'selected' : '' }}>822阿蓮區</option>
-                <option value="823" {{ old('area_code') == '823' ? 'selected' : '' }}>823田寮區</option>
-                <option value="824" {{ old('area_code') == '824' ? 'selected' : '' }}>824燕巢區</option>
-                <option value="825" {{ old('area_code') == '825' ? 'selected' : '' }}>825橋頭區</option>
-                <option value="826" {{ old('area_code') == '826' ? 'selected' : '' }}>826梓官區</option>
-                <option value="827" {{ old('area_code') == '827' ? 'selected' : '' }}>827彌陀區</option>
-                <option value="828" {{ old('area_code') == '828' ? 'selected' : '' }}>828永安區</option>
-                <option value="829" {{ old('area_code') == '829' ? 'selected' : '' }}>829湖內區</option>
-                <option value="830" {{ old('area_code') == '830' ? 'selected' : '' }}>830鳳山區</option>
-                <option value="831" {{ old('area_code') == '831' ? 'selected' : '' }}>831大寮區</option>
-                <option value="832" {{ old('area_code') == '832' ? 'selected' : '' }}>832林園區</option>
-                <option value="833" {{ old('area_code') == '833' ? 'selected' : '' }}>833鳥松區</option>
+                <option value="820" {{ $user->area_code == '820' ? 'selected' : '' }}>820岡山區</option>
+                <option value="821" {{ $user->area_code == '821' ? 'selected' : '' }}>821路竹區</option>
+                <option value="822" {{ $user->area_code == '822' ? 'selected' : '' }}>822阿蓮區</option>
+                <option value="823" {{ $user->area_code == '823' ? 'selected' : '' }}>823田寮區</option>
+                <option value="824" {{ $user->area_code == '824' ? 'selected' : '' }}>824燕巢區</option>
+                <option value="825" {{ $user->area_code == '825' ? 'selected' : '' }}>825橋頭區</option>
+                <option value="826" {{ $user->area_code == '826' ? 'selected' : '' }}>826梓官區</option>
+                <option value="827" {{ $user->area_code == '827' ? 'selected' : '' }}>827彌陀區</option>
+                <option value="828" {{ $user->area_code == '828' ? 'selected' : '' }}>828永安區</option>
+                <option value="829" {{ $user->area_code == '829' ? 'selected' : '' }}>829湖內區</option>
+                <option value="830" {{ $user->area_code == '830' ? 'selected' : '' }}>830鳳山區</option>
+                <option value="831" {{ $user->area_code == '831' ? 'selected' : '' }}>831大寮區</option>
+                <option value="832" {{ $user->area_code == '832' ? 'selected' : '' }}>832林園區</option>
+                <option value="833" {{ $user->area_code == '833' ? 'selected' : '' }}>833鳥松區</option>
 
-                <option value="840" {{ old('area_code') == '840' ? 'selected' : '' }}>840大樹區</option>
-                <option value="842" {{ old('area_code') == '842' ? 'selected' : '' }}>842旗山區</option>
-                <option value="843" {{ old('area_code') == '843' ? 'selected' : '' }}>843美濃區</option>
-                <option value="844" {{ old('area_code') == '844' ? 'selected' : '' }}>844六龜區</option>
-                <option value="845" {{ old('area_code') == '845' ? 'selected' : '' }}>845內門區</option>
-                <option value="846" {{ old('area_code') == '846' ? 'selected' : '' }}>846杉林區</option>
-                <option value="847" {{ old('area_code') == '846' ? 'selected' : '' }}>847甲仙區</option>
-                <option value="848" {{ old('area_code') == '848' ? 'selected' : '' }}>848桃源區</option>
-                <option value="849" {{ old('area_code') == '849' ? 'selected' : '' }}>849那瑪夏區</option>
-                <option value="851" {{ old('area_code') == '851' ? 'selected' : '' }}>851茂林區</option>
-                <option value="852" {{ old('area_code') == '852' ? 'selected' : '' }}>852茄萣區</option>
+                <option value="840" {{ $user->area_code == '840' ? 'selected' : '' }}>840大樹區</option>
+                <option value="842" {{ $user->area_code == '842' ? 'selected' : '' }}>842旗山區</option>
+                <option value="843" {{ $user->area_code == '843' ? 'selected' : '' }}>843美濃區</option>
+                <option value="844" {{ $user->area_code == '844' ? 'selected' : '' }}>844六龜區</option>
+                <option value="845" {{ $user->area_code == '845' ? 'selected' : '' }}>845內門區</option>
+                <option value="846" {{ $user->area_code == '846' ? 'selected' : '' }}>846杉林區</option>
+                <option value="847" {{ $user->area_code == '846' ? 'selected' : '' }}>847甲仙區</option>
+                <option value="848" {{ $user->area_code == '848' ? 'selected' : '' }}>848桃源區</option>
+                <option value="849" {{ $user->area_code == '849' ? 'selected' : '' }}>849那瑪夏區</option>
+                <option value="851" {{ $user->area_code == '851' ? 'selected' : '' }}>851茂林區</option>
+                <option value="852" {{ $user->area_code == '852' ? 'selected' : '' }}>852茄萣區</option>
             </select>
 
             <x-input-error class="mt-2" :messages="$errors->get('area_code')" /></div>
@@ -116,8 +116,8 @@
             <div class="w-1/6 pr-2">
             <label for="gender">性別</label>
             <select name="gender" id="gender" class="mt-1 block w-full shadow-sm border-gray-300 rounded-md">
-                <option value="男" {{ old('gender') == '男' ? 'selected' : '' }}>男</option>
-                <option value="女" {{ old('gender') == '女' ? 'selected' : '' }}>女</option>
+                <option value="男" {{ $user->gender == '男' ? 'selected' : '' }}>男</option>
+                <option value="女" {{ $user->gender == '女' ? 'selected' : '' }}>女</option>
             </select>
 
             <x-input-error class="mt-2" :messages="$errors->get('gender')" /></div>
